457c936ebe016dc97fe0198993f66bbea4e8f9a4
İlker Mavili <ilkermavili5369@hotmail.com>
2025-11-30 09:49:24 +0300

    test.rs

--- live-ticket-app/contracts/ticket-manager/src/test.rs
+++ live-ticket-app/contracts/ticket-manager/src/test.rs
@@ -1,11 +1,11 @@
-#![cfg(test)]
+/* #![cfg(test)]
 
 use super::*;
 use soroban_sdk::{vec, Env, String};
 
 #[test]
 fn test() {
-    /* 
+
     let env = Env::default();
     let contract_id = env.register(Contract, ());
     let client = ContractClient::new(&env, &contract_id);
@@ -19,5 +19,5 @@ fn test() {
             String::from_str(&env, "Dev"),
         ]
     );
-     */
 }
+*/
